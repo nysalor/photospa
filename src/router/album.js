e8f5f0266eebbdd85ece9e2eb162d1a39445dd7e
Jun Yokoyama <jun@larus.org>
2018-09-13 19:23:43 +0900

    index(Albums)及びalbum(Album)へのルーティングを追加した

--- src/router/album.js
+++ src/router/album.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Albums from '@/components/Albums'
+import Albums from '@/components/Album'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Albums',
-      component: Albums
+    {       
+      path: '/albums/:id',
+      name: 'Album',
+      component: Album
     }
   ]
 })
