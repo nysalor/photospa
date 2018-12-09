4a75b7f726f522010baa77d650f5eeaae3d4951c
Jun Yokoyama <jun@larus.org>
2018-12-10 00:38:01 +0900

    Album及びImageを追加した
    TODO:
    APIにimages/xxを追加する

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Albums from '@/components/Albums'
+import Album from '@/components/Album'
+import Image from '@/components/Image'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Albums',
       component: Albums
+    },
+    {
+      path: '/albums/:albumId',
+      name: 'Album',
+      component: Album
+    },
+    {
+      path: '/images/:imageId',
+      name: 'Image',
+      component: Image
     }
   ]
 })
